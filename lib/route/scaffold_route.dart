42e6926f3854d4597e2221d0d1003b0a37c69fb5
小撸 <lulin666000@gmail.com>
2023-05-01 09:57:04 +0800

    feat: scaffold

--- lib/route/scaffold_route.dart
+++ lib/route/scaffold_route.dart
@@ -15,6 +15,14 @@ class _ScaffoldRouteState extends State<ScaffoldRoute> {
     return Scaffold(
       appBar: AppBar(
         title: const Text("测试 Scaffold"),
+        leading: Builder(builder: (context) {
+          return IconButton(
+            icon: const Icon(Icons.dashboard, color: Colors.white),
+            onPressed: () {
+              Scaffold.of(context).openDrawer();
+            },
+          );
+        }),
         actions: [
           IconButton(
             onPressed: () {
@@ -30,34 +38,94 @@ class _ScaffoldRouteState extends State<ScaffoldRoute> {
           ),
         ],
       ),
-      drawer: MyDrawer(),
-      bottomNavigationBar: BottomNavigationBar(
-        items: const [
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.business), label: "Business"),
-          BottomNavigationBarItem(icon: Icon(Icons.school), label: "School"),
-        ],
-        currentIndex: _selectItemIndex,
-        fixedColor: Colors.blue,
-        onTap: (index) {
-          setState(() {
-            _selectItemIndex = index;
-          });
-        },
+      drawer: const MyDrawer(),
+      bottomNavigationBar: BottomAppBar(
+        shape: const CircularNotchedRectangle(),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.home),
+            ),
+            const SizedBox(
+              width: 40,
+              child: const Icon(Icons.add),
+            ),
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.business),
+            ),
+          ],
+        ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        child: const Icon(Icons.add),
-      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+
+      // bottomNavigationBar: BottomNavigationBar(
+      //   items: const [
+      //     BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+      //     BottomNavigationBarItem(
+      //         icon: Icon(Icons.business), label: "Business"),
+      //     BottomNavigationBarItem(icon: Icon(Icons.school), label: "School"),
+      //   ],
+      //   currentIndex: _selectItemIndex,
+      //   fixedColor: Colors.blue,
+      //   onTap: (index) {
+      //     setState(() {
+      //       _selectItemIndex = index;
+      //     });
+      //   },
+      // ),
+      // floatingActionButton: FloatingActionButton(
+      //   onPressed: () {},
+      //   child: const Icon(Icons.add),
+      // ),
     );
   }
 }
 
 class MyDrawer extends StatelessWidget {
+  const MyDrawer({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return Drawer(
+      child: MediaQuery.removePadding(
+        context: context,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(top: 38.0),
+              child: Row(
+                children: [
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 16.0),
+                    child: ClipOval(
+                      child: Image.asset("assets/images/avatar.png", width: 80),
+                    ),
+                  ),
+                  Text("sky", style: TextStyle(fontWeight: FontWeight.bold)),
+                ],
+              ),
+            ),
+            Expanded(
+              child: ListView(
+                children: const [
+                  ListTile(
+                    leading: Icon(Icons.add),
+                    title: Text("Add account"),
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.settings),
+                    title: Text("Manage accounts"),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
